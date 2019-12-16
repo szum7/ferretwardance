06ba1abec8d01f8f62bac3afc2527da8609067ee
Aron Szocs <arsz01@betssongroup.com>
2019-12-16 13:54:30 +0100

    filtering

--- ferretwardance/FerretWarDance.WebApp/ClientApp/src/app/components/home-tiles/reference-preview/reference-preview.component.ts
+++ ferretwardance/FerretWarDance.WebApp/ClientApp/src/app/components/home-tiles/reference-preview/reference-preview.component.ts
@@ -3,7 +3,13 @@ import { Component, OnInit } from '@angular/core';
 class Tile {
   url: string;
   title: string;
-  category: string;  
+  category: string;
+
+  constructor(url: string, title: string, category: string) {
+    this.url = url;
+    this.title = title;
+    this.category = category;
+  }
 }
 
 @Component({
@@ -18,27 +24,48 @@ export class ReferencePreviewComponent implements OnInit {
   urlBase: string;
   filter: string;
 
+  originalImages: Array<any>;
+  filteredImages: any;
+
   constructor() {
+    
+    this.filter = "all";
+
     this.urlBase = "../../../../assets/images/";
-    this.filter = "instagram";
-    this.images = [
-      [
-        {url: this.urlBase + "001.jpg", title: "Kép1"},
-        {url: this.urlBase + "002.jpg", title: "Kép2"}
-      ],
-      [
-        {url: this.urlBase + "003.jpg", title: "Kép3"}
-      ],
-      [        
-        {url: this.urlBase + "004.jpeg", title: "Kép4"},
-      ]
+
+    this.originalImages = [
+      new Tile(this.urlBase + "001.jpg", "", "photo"),
+      new Tile(this.urlBase + "002.jpg", "", "photo"),
+      new Tile(this.urlBase + "003.jpg", "", "painting"),
+      new Tile(this.urlBase + "004.jpeg", "", "photo"),
+      new Tile(this.urlBase + "005.jpg", "", "photo"),
+      new Tile(this.urlBase + "006.jfif", "", "painting"),
+      new Tile(this.urlBase + "007.jpg", "", "photo"),
+      new Tile(this.urlBase + "008.jpg", "", "painting"),
+      new Tile(this.urlBase + "009.png", "", "painting")
     ];
-    this.imageList = [
-      {url: this.urlBase + "001.jpg", title: "Kép1"},
-      {url: this.urlBase + "002.jpg", title: "Kép2"},
-      {url: this.urlBase + "003.jpg", title: "Kép3"},
-      {url: this.urlBase + "004.jpeg", title: "Kép4"}
-    ];
+
+
+    this.buildFilteredImages();
+
+    // this.images = [
+    //   [
+    //     new Tile(this.urlBase + "001.jpg", "", "photo"),
+    //     new Tile(this.urlBase + "002.jpg", "", "photo")
+    //   ],
+    //   [
+    //     new Tile(this.urlBase + "003.jpg", "", "painting")
+    //   ],
+    //   [
+    //     new Tile(this.urlBase + "004.jpeg", "", "painting")
+    //   ]
+    // ];
+    // this.imageList = [
+    //   {url: this.urlBase + "001.jpg", title: "Kép1"},
+    //   {url: this.urlBase + "002.jpg", title: "Kép2"},
+    //   {url: this.urlBase + "003.jpg", title: "Kép3"},
+    //   {url: this.urlBase + "004.jpeg", title: "Kép4"}
+    // ];
   }
   
   ngOnInit(): void {
@@ -46,6 +73,46 @@ export class ReferencePreviewComponent implements OnInit {
 
   setFilter(val: string): void{
     this.filter = val;
+    this.buildFilteredImages();
   }
+
+
+  private orderImageList(): void {
+    // Check page width -> 2 | 3 columns
+    // Order by 'order' property
+    // Copy it to property
+  }
+
+  buildFilteredImages(): void {
+
+    this.filteredImages = [[], [], []];
+
+    for (let i = 0, o = 0; i < this.originalImages.length; i++) {
+
+      const el = this.originalImages[i];
+      console.log(this.filter);
+
+      if (this.filter !== "all" && el.category !== this.filter) {
+        continue;
+      }
+
+      this.filteredImages[o].push(new Tile(
+        el.url, 
+        el.title, 
+        el.category)
+      );
+
+      if (o == 2) {
+        o = 0;
+      } else {
+        o++;
+      }
+    }
+
+    // Filter
+    // Copy to property
+  }
+
+
 
 }
