0a1ba0f66a77ca656653c8420980e0a77daef20a
arsz01 <aron.szocs@betssongroup.com>
2019-12-23 10:12:45 +0100

    Grid class

--- ferretwardance/FerretWarDance.WebApp/ClientApp/src/app/components/home-tiles/reference-preview/reference-preview.component.ts
+++ ferretwardance/FerretWarDance.WebApp/ClientApp/src/app/components/home-tiles/reference-preview/reference-preview.component.ts
@@ -1,15 +1,71 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, HostListener } from '@angular/core';
 
 class Tile {
   url: string;
   title: string;
   category: string;
+  order: number;
 
-  constructor(url: string, title: string, category: string) {
+  constructor(url: string, title: string, category: string, order: number) {
     this.url = url;
     this.title = title;
     this.category = category;
+    this.order = order;
   }
+}
+
+class Grid {
+  
+  public originalTiles: Array<Tile>;
+  private filteredTiles: Array<Tile>;
+  public columns: Array<Array<Tile>>;
+  public filter: string;
+
+  constructor() {
+    this.resetColumns();
+    this.filteredTiles = [];
+    this.filter = 'all';
+  }
+
+  buildTiles(): void {
+    this.resetColumns();
+    this.filterTiles();
+    this.orderTiles();
+    this.spreadTiles(this.filteredTiles, this.columns);
+  }
+
+  private resetColumns(): void {
+    this.columns = [[], [], []];
+  }
+
+  private orderTiles(): void {
+    this.filteredTiles.sort((tile1, tile2) => (tile1.order > tile2.order) ? 1 : -1);    
+  }
+
+  private filterTiles(): void {
+    if (this.filter === 'all') {
+      this.filteredTiles = this.originalTiles;
+    } else {
+      this.filteredTiles = this.originalTiles.filter(tile => tile.category === this.filter);
+    }
+  }
+
+  private spreadTiles(source: Array<Tile>, destination: Array<Array<Tile>>): void {
+    for (let i = 0, o = 0; i < source.length; i++) {
+
+      const el = source[i];
+
+      destination[o].push(new Tile(
+        el.url, 
+        el.title, 
+        el.category,
+        el.order)
+      );
+
+      o = (o == 2) ? 0 : o + 1;
+    }
+  }
+
 }
 
 @Component({
@@ -24,8 +80,8 @@ export class ReferencePreviewComponent implements OnInit {
   urlBase: string;
   filter: string;
 
-  originalImages: Array<any>;
-  filteredImages: any;
+  originalImages: Array<Tile>;
+  filteredImages: any;  
 
   constructor() {
     
@@ -34,15 +90,15 @@ export class ReferencePreviewComponent implements OnInit {
     this.urlBase = "../../../../assets/images/";
 
     this.originalImages = [
-      new Tile(this.urlBase + "001.jpg", "", "photo"),
-      new Tile(this.urlBase + "002.jpg", "", "photo"),
-      new Tile(this.urlBase + "003.jpg", "", "painting"),
-      new Tile(this.urlBase + "004.jpeg", "", "photo"),
-      new Tile(this.urlBase + "005.jpg", "", "photo"),
-      new Tile(this.urlBase + "006.jfif", "", "painting"),
-      new Tile(this.urlBase + "007.jpg", "", "photo"),
-      new Tile(this.urlBase + "008.jpg", "", "painting"),
-      new Tile(this.urlBase + "009.png", "", "painting")
+      new Tile(this.urlBase + "001.jpg", "", "photo", 1),
+      new Tile(this.urlBase + "002.jpg", "", "photo", 2),
+      new Tile(this.urlBase + "003.jpg", "", "painting", 3),
+      new Tile(this.urlBase + "004.jpeg", "", "photo", 4),
+      new Tile(this.urlBase + "005.jpg", "", "photo", 5),
+      new Tile(this.urlBase + "006.jfif", "", "painting", 6),
+      new Tile(this.urlBase + "007.jpg", "", "photo", 7),
+      new Tile(this.urlBase + "008.jpg", "", "painting", 8),
+      new Tile(this.urlBase + "009.png", "", "painting", 9)
     ];
 
 
@@ -69,6 +125,12 @@ export class ReferencePreviewComponent implements OnInit {
   }
   
   ngOnInit(): void {
+    
+  }
+
+  @HostListener('window:resize', ['$event'])
+  onResize(event) {
+    console.log(window.innerWidth);
   }
 
   setFilter(val: string): void{
@@ -90,7 +152,6 @@ export class ReferencePreviewComponent implements OnInit {
     for (let i = 0, o = 0; i < this.originalImages.length; i++) {
 
       const el = this.originalImages[i];
-      console.log(this.filter);
 
       if (this.filter !== "all" && el.category !== this.filter) {
         continue;
@@ -99,14 +160,11 @@ export class ReferencePreviewComponent implements OnInit {
       this.filteredImages[o].push(new Tile(
         el.url, 
         el.title, 
-        el.category)
+        el.category,
+        el.order)
       );
 
-      if (o == 2) {
-        o = 0;
-      } else {
-        o++;
-      }
+      o = (o == 2) ? 0 : o + 1;
     }
 
     // Filter
